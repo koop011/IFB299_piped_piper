7656853ae52b8e6612dc4771c07b9da7d02597cf
Leo <leo.sj.yoon@gmail.com>
2018-04-04 21:47:55 +1000

    Created virtual env

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pc\dropbox\uni\2018\sem1\ifb299\ifb299_git\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
