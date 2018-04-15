41331d03f6e3628120aae1d6e4f39babe9df8114
Leo <leo.sj.yoon@gmail.com>
2018-04-15 11:46:56 +1000

    Structure fixed

--- Project/IFB299_piped_piper/urls.py
+++ Project/IFB299_piped_piper/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('welcome_page.urls')),
+    #path('home/', include('welcome_page.urls')),
 ]
--- Project/welcome_page/views.py
+++ Project/welcome_page/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 import templates
-from templates import templates
 
 def index(request):
-    return render(request, 'templates/welcome_page/home.html')
+    return render(request, 'templates.welcome_page.home.html')
