4ac99c4ff55bbc6b0164062e4e6c4e3267121b95
DESKTOP-GP2BMJG\Braindead <christopher.haywood91@gmail.com>
2018-04-15 11:24:22 +1000

    Structure edits for leo

--- Project/welcome_page/views.py
+++ Project/welcome_page/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from . import templates
+import templates
+from templates import templates
 
 def index(request):
     return render(request, 'templates/welcome_page/home.html')
