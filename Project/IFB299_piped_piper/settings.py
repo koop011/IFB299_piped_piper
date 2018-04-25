c1e4a16258da8dba480fbd1745266e4d7db23c69
Leo <leo.sj.yoon@gmail.com>
2018-04-25 20:05:06 +1000

    Removed unecessary student_login directory as loginPage already exists.
    Created a database and managed to validate a dummy user through the login interface using django authentication.

--- Project/IFB299_piped_piper/settings.py
+++ Project/IFB299_piped_piper/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'student_login',
+    'loginPage.apps.LoginPageConfig',
     'welcome_page',
     'django.contrib.admin',
     'django.contrib.auth',
--- Project/student_login/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StudentLoginConfig(AppConfig):
-    name = 'student_login'
