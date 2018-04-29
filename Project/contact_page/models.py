dc5ddd90fabbbdb060ed5c0e45ce15804a61db77
COMPUTER\ramon <n10014594@qut.edu.au>
2018-04-30 02:19:32 +1000

    was doing stuff related more to file upload than testing so commiting to switch

--- Project/contact_page/models.py
+++ Project/contact_page/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 from django import forms
+
+
 class TeacherData(models.Model):
     FullName = models.CharField(max_length=200)
     DoB = models.CharField(max_length=20)
