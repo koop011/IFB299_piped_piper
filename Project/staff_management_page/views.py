cbab21c2adf7ccec466d0a13da593615755a069c
Desktop\Rory <roryjseiffe@gmail.com>
2018-05-01 21:23:24 +1000

    FIVE-31 User story validation

--- Project/staff_management_page/views.py
+++ Project/staff_management_page/views.py
@@ -6,3 +6,22 @@ def staff_managementIndex(request):
     context = {}
     return render(request, 'Staff_management_page/staff_management_page.html', context)
 
+def comingSoon(request):
+    context = {}
+    return render(request, 'reportGeneration/comingSoon.html', context)
+
+def comingSoon(request):
+    context = {}
+    return render(request, 'editTheDatabase/comingSoon.html', context)
+
+def comingSoon(request):
+    context = {}
+    return render(request, 'postEvent/comingSoon.html', context)
+
+def comingSoon(request):
+    context = {}
+    return render(request, 'contractApproval/comingSoon.html', context)
+
+def comingSoon(request):
+    context = {}
+    return render(request, 'StaffManagement/comingSoon.html', context)
