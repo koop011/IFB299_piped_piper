58b06a6763e1667353b7d25b6ce4a4a7f408b0c0
DESKTOP-GP2BMJG\Braindead <christopher.haywood91@gmail.com>
2018-05-20 20:51:39 +1000

    Added search and filter functions to student account database

--- Project/student_account/admin.py
+++ Project/student_account/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import StudentData
 
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('FirstName', 'LastName', 'UserName', 'DoB', 'Gender', 'Email' , 'pNumber')
+    list_filter = ('Gender', 'DoB')
+    search_fields = ('FirstName', 'LastName', 'UserName')
 
-admin.site.register(StudentData)
+
+admin.site.site_header = 'Administration'
+admin.site.register(StudentData, StudentAdmin)
--- Project/student_account/models.py
+++ Project/student_account/models.py
@@ -1,27 +1,33 @@
 from django.db import models
 from django.urls import reverse
 
-GENDER_CHOICES= [
+GENDER_CHOICES = [
     ('Male', 'Male'),
     ('Female', 'Female'),
     ('Other', 'Other'),
 ]
+
 
 class StudentData(models.Model):
     FirstName = models.CharField(max_length=200, default='none')
     LastName = models.CharField(max_length=200, default='none')
     UserName = models.CharField(max_length=20, default='none')
     password = models.CharField(max_length=20, default='none')
-    GenderRadioOptions = models.CharField(max_length=7, choices=GENDER_CHOICES, default='Other')
+    Gender = models.CharField(max_length=7, choices=GENDER_CHOICES, default='Other')
     DoB = models.CharField(max_length=20, default='none')
     HomeAddress = models.CharField(max_length=200, default='none')
     Email = models.CharField(max_length=200, default='none')
     pNumber = models.CharField(max_length=13, default='none')
     subjects = models.CharField(max_length=200, default='none')
+    pFirstName = models.CharField(max_length=200, default='none')
+    pLastName = models.CharField(max_length=200, default='none')
+    parentsNumber = models.CharField(max_length=13, default='none')
+    pEmail = models.CharField(max_length=200, default='none')
 
     def __str__(self):
-        return self.FirstName +(' - ') + self.LastName +(' - ') + self.GenderRadioOptions +(' - ') + self.DoB  +(' - ') + self.HomeAddress +(' - ') + self.Email +(' - ') + self.pNumber
-
+        return self.FirstName
+               # + (' - ') + self.LastName + (' - ') + self.GenderRadioOptions + (' - ') + self.DoB + (
+            # ' - ') + self.HomeAddress + (' - ') + self.Email + (' - ') + self.pNumber
 
     def get_absolute_url(self):
         return reverse('student_account:student_account.html')
